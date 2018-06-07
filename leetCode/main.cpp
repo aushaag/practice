d686a30c6085387ee1c74ab51ceaf6e2eae414e0
Austin Haag <ahaag10@lamu02vx0lhhtd6.uhc.com>
2018-06-07 15:56:57 -0500

    Initial Commit

---
+++ leetCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leetCode
+//
+//  Created by Haag, Austin P on 6/7/18.
+//  Copyright © 2018 Haag, Austin P. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
