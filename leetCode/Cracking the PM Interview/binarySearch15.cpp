1dbd84ad43b61bdcf81ee71bb49d4cf30acbf1d4
Austin Haag <aushaag@umich.edu>
2018-07-23 11:43:00 -0500

    Adding remaining two problems

---
+++ leetCode/Cracking the PM Interview/binarySearch15.cpp
@@ -0,0 +1,23 @@
+//
+//  binarySearch15.cpp
+//  leetCode
+//
+//  Created by Austin Haag on 7/20/18.
+//  Copyright © 2018 Haag, Austin P. All rights reserved.
+//
+
+#include "libraries.h"
+
+
+// 16.15
+// Implement binary search. That is, given a sorted array of integers and a value, find the location of that value.
+
+void binarySearch(int array[], int x) {
+    
+}
+
+int main() {
+    
+    cout << endl;
+}
+
---
+++ leetCode/Cracking the PM Interview/stringPermutations20.cpp
@@ -0,0 +1,24 @@
+//
+//  stringPermutations.cpp
+//  leetCode
+//
+//  Created by Austin Haag on 7/20/18.
+//  Copyright © 2018 Haag, Austin P. All rights reserved.
+//
+
+#include "libraries.h"
+
+// 16.20
+// Given a string, print all permutations of that string. You can assume the word does not have any duplicate characters.
+
+
+bool permutations(string word) {
+    
+    return false;
+}
+
+int main() {
+    
+    cout << endl;
+}
+
