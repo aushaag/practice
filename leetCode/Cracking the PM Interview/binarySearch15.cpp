7021f404f8f833e257c2723445285fac1818fff6
ahaag10 <austin.haag@optum.com>
2018-08-01 16:31:20 -0500

    Solved 14 and 15

--- leetCode/Cracking the PM Interview/binarySearch15.cpp
+++ leetCode/Cracking the PM Interview/binarySearch15.cpp
@@ -12,9 +12,43 @@
 // 16.15
 // Implement binary search. That is, given a sorted array of integers and a value, find the location of that value.
 
-void binarySearch(int array[], int x) {
-    
+int binarySearchHelper(int arr[], int x, int left, int right) {
+    if (left > right) return -1; // not found
+    int middle = (left + right) / 2;
+    if (x == arr[middle]) return middle;
+    // x in left half
+    else if (x < arr[middle]) {
+        return binarySearchHelper(arr, x, left, middle - 1);
+    } else {
+        return binarySearchHelper(arr, x, middle + 1, right);
+    }
 }
+
+int binarySearch(int arr[], int size, int x) {
+    return binarySearchHelper(arr, x, 0, size - 1);
+}
+
+// iterative version
+
+int iterativeBinarySearch(int arr[], int size, int x) {
+    int right = size - 1;
+    int left = 0;
+    int middle;
+    
+    while (left <= right) {
+        middle = (left + right) / 2;
+        if (arr[middle] == x) return middle;
+        // x in left half
+        else if (x < arr[middle]) {
+            right = middle - 1;
+        } else {
+            left = middle + 1;
+        }
+    }
+    // not found
+    return -1;
+}
+
 
 int main() {
     
--- leetCode/Cracking the PM Interview/insertionSort14.cpp
+++ leetCode/Cracking the PM Interview/insertionSort14.cpp
@@ -12,11 +12,52 @@
 // 16.14
 // Implement insertion sort.
 
-void insertionSort(int array[]) {
+void insertionSort(int array[], int size) {
     
+    int left = 0;
+    int right = 1;
+    
+    while (right < size) {
+        left = 0;
+        // find place to swap
+        while (array[left] < array[right] && left != right) {
+            ++left;
+        }
+        swap(array[left], array[right]);
+        ++right;
+    }
 }
 
+
+void printArr(int arr[], int size) {
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+
 int main() {
+    
+    int A[5] = { 6, 5, 0, -1, 4};
+    int sizeA = 5;
+    
+    int B[5] = { 0, 1, 3, 5, 7};
+    int sizeB = 5;
+    
+    int C[0];
+    int sizeC = 0;
+    
+    insertionSort(A, sizeA);
+    printArr(A, sizeA);
+    
+    insertionSort(B, sizeB);
+    printArr(B, sizeB);
+    
+    insertionSort(C, 0);
+    printArr(C, sizeC);
+    
+    
     
     cout << endl;
 }
